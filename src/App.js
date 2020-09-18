d46dad1cbc31141499ddd0cd2ffdbbe1463b81f3
Leonardo Schmitt <Leoeyeschmittk@gmail.com>
2020-09-18 11:28:03 -0300

    givin the code a prettier style

--- src/App.js
+++ src/App.js
@@ -1,58 +1,52 @@
 import React, { Component } from "react";
 
+class ChangeColor extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      color: "#fafafa",
+    };
+  };
 
-class ChangeColor extends React.Component{
-  constructor(){
-   super()
-   this.state={
-     color:'#fafafa'
-   }
-  }
- handleChange(event){
+  handleChange(event) {
     this.setState({
-     color:event.target.value
-    })
- } 
+      color: event.target.value,
+    });
+  };
 
+  render() {
+    const styleContainerInput = {
+      width: 400,
+      height: 400,
+      background: this.state.color,
+      borderRadius: "50%",
+      margin: "auto",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      position: "relative",
+      boxShadow: "inset 0 10px 70px #d6d2d2",
+    };
+    const styleTextColor = {
+      position: "absolute",
+      fontFamily: "cursive",
+    };
+    return (
+      <div style={styleContainerInput}>
+        <h1 style={styleTextColor}>{this.state.color}</h1>
+        <input
+          type="color"
+          value={this.state.color}
+          style={{ margin: "auto", width: 150, height: 150, padding: 10 }}
+          onChange={this.handleChange.bind(this)}
+        />
+      </div>
+    );
+  };
+};
 
+function App() {
+  return <ChangeColor />;
+};
 
-
-  render(){
-
-
-   const styleContainerInput={
-    width:400,
-    height:400,
-    background:this.state.color, 
-    // border:'1px solid grey',
-    borderRadius:'50%',
-    margin:'auto',
-    display:'flex',
-    justifyContent:'center',
-    alignItems:'center',
-    position:'relative',
-    boxShadow:'inset 0 10px 70px #d6d2d2'
-  }
-  const styleTextColor={
-    position:'absolute',
-    fontFamily:'cursive',
-  }
-   return (
-    <div style={styleContainerInput}>
-     <h1 style={styleTextColor}>{this.state.color}</h1>
-       <input type='color' value={this.state.color} style={{margin:'auto',width:150,height:150,padding:10}} onChange={this.handleChange.bind(this)} />
-       
-       {/* <h1 style={}> {window.document.body.style.backgroundColor=this.state.color}</h1> */}
-    </div>
-          
-    )
-  }
-}
-
- function App() {
- return(
-   <ChangeColor />
- )
-}
-
-export default App
+export default App;
